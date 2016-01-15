e764107ac784dda849ddedb5171dbbe61ab17ba5
GeorgeKaraxalios <george.kar@outlook.com>
2016-01-15 13:51:08 +0200

    Create ConnectorForAdeies.php

---
+++ ConnectorForAdeies.php
@@ -0,0 +1,21 @@
+<?php
+class ConnectorForAdeies{
+        public $dbh;
+        public $stmt;
+        public function __construct(){
+                $this->dbh=new PDO('mysql:dbname=adeies;host=localhost','root','');
+                $this->dbh -> exec("set names utf8");
+//                $this->dbh -> exec("SET CHARACTER SET 'utf8'");
+        }
+
+        public function prepare($query,array $fields=null){
+                $this->stmt = $this->dbh->prepare($query);
+                if($fields!==null){
+                        for($i=0;$i<sizeof($fields);$i++)
+                                $this->stmt->bindParam($i+1,$fields[$i]);
+                }
+                $this->stmt->execute();
+                return $this->stmt;
+                }
+        }
+?>
